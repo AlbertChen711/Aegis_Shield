590a8f69ab16267c15d7f1551bef3da04c37f728
warrensze <warrensze1@yahoo.com>
2026-07-25 16:15:41 -0700

    better ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -223,6 +223,7 @@ export default function App() {
         <div className="sunbeam sunbeam-2"></div>
         <div className="sunbeam sunbeam-3"></div>
         <div className="sunbeam sunbeam-4"></div>
+        <div className="sunbeam sunbeam-5"></div>
         <div className="cloud cloud-1"><div className="cloud-core"></div></div>
         <div className="cloud cloud-2"><div className="cloud-core"></div></div>
         <div className="cloud cloud-3"><div className="cloud-core"></div></div>
